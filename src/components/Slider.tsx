ed18e047fab611e4445941ca00f5af1f22248cdf
Raul Sigoli <raul_sigoli@hotmail.com>
2024-01-08 01:22:32 -0300

    feat: pegando todas as imagens da pasta para o slider

--- src/components/Slider.tsx
+++ src/components/Slider.tsx
@@ -1,19 +1,16 @@
 import React, { useEffect, useState } from "react";
-import Image, { StaticImageData } from "next/image";
-
-import image1 from "/public/image/Celso Pilati -0007.jpg";
-import image2 from "/public/image/Celso Pilati -0016.jpg";
-import image3 from "/public/image/Celso Pilati -0019.jpg";
-import image4 from "/public/image/Celso Pilati -0041.jpg";
+import Image from "next/image";
 
 export default function Slider() {
-  const images: StaticImageData[] = [image1, image2, image3, image4];
+  // Importa todas as imagens da pasta /public/image/ que têm a extensão .jpg
+  const images = require.context("/public/image", false, /\.jpg$/);
+  const imageKeys = images.keys();
+
   const [currentImageIndex, setCurrentImageIndex] = useState<number>(0);
 
   // Função para avançar para a próxima imagem
-  // eslint-disable-next-line react-hooks/exhaustive-deps
   const nextImage = () => {
-    setCurrentImageIndex((prevIndex) => (prevIndex + 1) % images.length);
+    setCurrentImageIndex((prevIndex) => (prevIndex + 1) % imageKeys.length);
   };
 
   useEffect(() => {
@@ -24,14 +21,14 @@ export default function Slider() {
 
     // Limpe o intervalo quando o componente for desmontado
     return () => clearInterval(interval);
-  }, [currentImageIndex, nextImage]);
+  }, [currentImageIndex]);
 
   return (
     <div className="relative h-screen">
       <div className="absolute inset-0 z-10 transition-opacity duration-3000">
         <Image
-          key={images[currentImageIndex].src} // Use o caminho da imagem como chave
-          src={images[currentImageIndex]}
+          key={imageKeys[currentImageIndex]}
+          src={images(imageKeys[currentImageIndex])}
           layout="fill"
           objectFit="cover"
           alt="Descrição da imagem"
@@ -41,15 +38,13 @@ export default function Slider() {
 
       <div className="absolute inset-0 bg-black opacity-50 z-20"></div>
       <div className="absolute inset-0 z-30 flex items-center justify-center">
-      <div className="mx-auto max-w-screen-xl px-4 py-32 lg:flex lg:h-screen lg:items-center">
+        <div className="mx-auto max-w-screen-xl px-4 py-32 lg:flex lg:h-screen lg:items-center">
           <div className="mx-auto max-w-xl text-center">
-          
-
             <ul className="mt-8 flex flex-wrap justify-center gap-4">
               <li>
                 <a
                   className="text-white  transition hover:text-white/75 uppercase"
-                  href="#"
+                  href="/projetos"
                 >
                   PROJETOS
                 </a>
@@ -58,7 +53,7 @@ export default function Slider() {
               <li>
                 <a
                   className="text-white transition hover:text-white/75 uppercase"
-                  href="#"
+                  href="/sobre"
                 >
                   SOBRE
                 </a>
@@ -67,7 +62,7 @@ export default function Slider() {
               <li>
                 <a
                   className="text-white transition hover:text-white/75 uppercase"
-                  href="#"
+                  href="/midia"
                 >
                   MÍDIA
                 </a>
@@ -76,7 +71,7 @@ export default function Slider() {
               <li>
                 <a
                   className="text-white transition hover:text-white/75 uppercase"
-                  href="#"
+                  href="/contato"
                 >
                   CONTATO
                 </a>
--- src/pages/projetos.tsx
+++ src/pages/projetos.tsx
@@ -7,7 +7,7 @@ export default function Home() {
   
   return (
    <>
-    <Slider />
+   <h1>dasdasd</h1>
    </>
   );
 }
