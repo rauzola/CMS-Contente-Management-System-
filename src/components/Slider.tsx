6541c9714ec6ca3cb3dee1f114e637150e8d3074
Raul Sigoli <raul_sigoli@hotmail.com>
2024-01-08 12:41:19 -0300

    fix: ajustando as imagens

--- src/components/Slider.tsx
+++ src/components/Slider.tsx
@@ -1,16 +1,23 @@
+/* eslint-disable react-hooks/exhaustive-deps */
 import React, { useEffect, useState } from "react";
-import Image from "next/image";
+import Image, { StaticImageData } from "next/image";
+import Link from "next/link";
+
+import image1 from "/public/image/Celso Pilati -0007.jpg";
+import image2 from "/public/image/Celso Pilati -0016.jpg";
+import image3 from "/public/image/Celso Pilati -0019.jpg";
+import image4 from "/public/image/Celso Pilati -0041.jpg";
 
 export default function Slider() {
   // Importa todas as imagens da pasta /public/image/ que têm a extensão .jpg
-  const images = require.context("/public/image", false, /\.jpg$/);
+  const images: StaticImageData[] = [image1, image2, image3, image4];
   const imageKeys = images.keys();
 
   const [currentImageIndex, setCurrentImageIndex] = useState<number>(0);
 
   // Função para avançar para a próxima imagem
   const nextImage = () => {
-    setCurrentImageIndex((prevIndex) => (prevIndex + 1) % imageKeys.length);
+    setCurrentImageIndex((prevIndex) => (prevIndex + 1) % images.length);
   };
 
   useEffect(() => {
@@ -27,8 +34,8 @@ export default function Slider() {
     <div className="relative h-screen">
       <div className="absolute inset-0 z-10 transition-opacity duration-3000">
         <Image
-          key={imageKeys[currentImageIndex]}
-          src={images(imageKeys[currentImageIndex])}
+          key={images[currentImageIndex].src} // Use o caminho da imagem como chave
+          src={images[currentImageIndex]}
           layout="fill"
           objectFit="cover"
           alt="Descrição da imagem"
@@ -42,39 +49,39 @@ export default function Slider() {
           <div className="mx-auto max-w-xl text-center">
             <ul className="mt-8 flex flex-wrap justify-center gap-4">
               <li>
-                <a
+                <Link
                   className="text-white  transition hover:text-white/75 uppercase"
                   href="/projetos"
                 >
                   PROJETOS
-                </a>
+                </Link>
               </li>
 
               <li>
-                <a
+                <Link
                   className="text-white transition hover:text-white/75 uppercase"
                   href="/sobre"
                 >
                   SOBRE
-                </a>
+                </Link>
               </li>
 
               <li>
-                <a
+                <Link
                   className="text-white transition hover:text-white/75 uppercase"
                   href="/midia"
                 >
                   MÍDIA
-                </a>
+                </Link>
               </li>
 
               <li>
-                <a
+                <Link
                   className="text-white transition hover:text-white/75 uppercase"
                   href="/contato"
                 >
                   CONTATO
-                </a>
+                </Link>
               </li>
             </ul>
           </div>
